025363cdf8dfe3056bdaa5a36948d1914b0dd601
Mario Gonzalez <marioalglez@gmail.com>
2025-08-14 16:53:59 -0600

    fix env on front

--- ekilibria/frontend/ekilibria-front/vite.config.js
+++ ekilibria/frontend/ekilibria-front/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig(({ mode }) => {
+  // Load env file from current directory
+  const env = loadEnv(mode, '../../../', '')
+  
+  return {
+    plugins: [svelte()],
+    define: {
+      // Make environment variables available
+      'import.meta.env.VITE_ENV': JSON.stringify(env.VITE_ENV || 'development')
+    }
+  }
 })
